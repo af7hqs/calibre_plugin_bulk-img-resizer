9d63e9f3be0fb4d4ca7f6fe4b0029e110150f685
af7hqs <58590100+af7hqs@users.noreply.github.com>
2025-05-29 14:11:55 +0200

    Update image.py

--- image.py
+++ image.py
@@ -2,7 +2,7 @@ import io
 from PIL import Image
 
 
-def compress_image(img_data, max_px_size, quality, encoding_type):
+def compress_image(img_data, max_px_size, quality, encoding_type, algorithm, blur_radius):
     image = Image.open(io.BytesIO(img_data))
     w = float(image.size[0])
     h = float(image.size[1])
@@ -23,7 +23,26 @@ def compress_image(img_data, max_px_size, quality, encoding_type):
             new_h = h
             new_w = w
 
-    re_image = image.resize((int(new_w), int(new_h)), Image.Resampling.LANCZOS)
+    if blur_radius == 0:
+        blurred_image = image
+    else:
+        blurred_image = image.filter(ImageFilter.GaussianBlur(radius=blur_radius))
+        
+    if algorithm == 'BOX':
+        re_image = blurred_image.resize((int(new_w), int(new_h)), Image.Resampling.BOX)
+    else if algorithm == 'BILINEAR':
+        re_image = blurred_image.resize((int(new_w), int(new_h)), Image.Resampling.BILINEAR)
+    else if algorithm == 'BICUBIC':
+        re_image = blurred_image.resize((int(new_w), int(new_h)), Image.Resampling.BICUBIC)
+    else if algorithm == 'HAMMING':
+        re_image = blurred_image.resize((int(new_w), int(new_h)), Image.Resampling.HAMMING)
+    else if algorithm == 'NEAREST':
+        re_image = blurred_image.resize((int(new_w), int(new_h)), Image.Resampling.NEAREST)
+    else if algorithm == 'LANCZOS':
+        re_image = blurred_image.resize((int(new_w), int(new_h)), Image.Resampling.LANCZOS)
+    else:
+        re_image = blurred_image.resize((int(new_w), int(new_h)), Image.Resampling.BILINEAR)
+
     buf = io.BytesIO()
 
     if encoding_type == 'WebP':
